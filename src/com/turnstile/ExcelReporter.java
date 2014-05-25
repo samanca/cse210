3ce5d4f126e4af3fb28d17b8a616cbc49cc22743
Raul <ra_go_hu@hotmail.com>
2014-05-24 22:16:27 -0700

    Added generateMonth method in TSheet.java and modified Export in ExcelReporter.java

--- src/com/turnstile/ExcelReporter.java
+++ src/com/turnstile/ExcelReporter.java
@@ -3,6 +3,7 @@ package com.turnstile;
 import java.io.File;
 import jxl.*;
 import jxl.write.*;
+import jxl.write.Number;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -52,12 +53,29 @@ public class ExcelReporter {
                     //TODO handle this
                 }
             }
+            
+            // Add row for total
+            if (rawSheet.isSummary) {
+            	Label t = new Label(0, rawSheet.columns.length + 1, "Total");
+                try {
+                    sheet.addCell(t);
+                }
+                catch (WriteException ex) {
+                    //TODO handle this
+                }
+            } 
 
             // Export data
             int rowNumber = 1;
             for(String[] row : rawSheet.rows) {
                 for(int i = 0; i < row.length; i++) {
-                    Label t = new Label(rowNumber, i, row[i]);
+                    WritableCell t;
+                    if (!rawSheet.isSummary) {
+                    	t = new Number(rowNumber, i, Double.parseDouble(row[i]));
+                    }
+                    else {
+                    	t = new Formula(rowNumber, i, row[i]);
+                    }
                     try {
                         sheet.addCell(t);
                     }
