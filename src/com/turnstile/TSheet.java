659f8c2870b968aebf549ccd2514a56e59e01b6f
Raul <ra_go_hu@hotmail.com>
2014-06-08 20:31:06 -0700

    Resolves #12 and also adds indeterminate counts

--- src/com/turnstile/TSheet.java
+++ src/com/turnstile/TSheet.java
@@ -1,7 +1,6 @@
 package com.turnstile;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 public class TSheet {
     public ArrayList<String[]> rows;
@@ -10,7 +9,8 @@ public class TSheet {
             "Perm",
             "Alumni",
             "Nonresident",
-            "HVRP"
+            "HVRP",
+            "Indeterminate"
     };
     public String label;
     public boolean isSummary = false;
@@ -36,8 +36,9 @@ public class TSheet {
     	for (int i = 1; i < data.length; i++) {
     		String[] t = new String[sheet.columns.length + 1];
             t[0] = String.valueOf(i);
-            for(int j = 1; j < t.length; j++)
+            for(int j = 1; j < t.length - 1; j++)
                 t[j] = "" + data[i][j];
+            t[t.length - 1] = "" + data[i][0];
             sheet.rows.add(t);
     	}
     	retVal.add(sheet);
@@ -46,29 +47,26 @@ public class TSheet {
     	sheet = new TSheet();
         sheet.label = "Summary";
         sheet.isSummary = true;
+        
+        int errors = 0;
+        // Add up the errors for incorrect dates
+        for (int i = 0; i < data[0].length; i++) {
+            errors += data[0][i];
+        }
 
         // Add formulas for each resident type
         String[] t = new String[sheet.columns.length + 2];
         t[0] = "1";
         for(int j = 1; j < t.length - 1; j++) {
-            t[j] = generateSumFormula(month, j + 1);
+            t[j] = generateSumFormula(month, j, 2, 32);
+            
+            if (j == t.length - 2) {
+                t[j] = errors + "+" + t[j];
+            }
         }
-        
-        
-        int errors = 0;
-        
-        // Add up the errors for each day
-        for (int i = 1; i < data.length; i++) {
-        	errors += data[i][0];
-        }
-        
-        // Add up the errors for incorrect dates
-        for (int i = 0; i < data[0].length; i++) {
-        	errors += data[0][i];
-        }
-        
+
         // Formula for Total
-        t[t.length - 1] = errors + " + SUM(B2:B" + (sheet.columns.length)+ ")";
+        t[t.length - 1] = "SUM(B2:B5, B7)";
         sheet.rows.add(t);
         
         retVal.add(sheet);
@@ -83,22 +81,17 @@ public class TSheet {
      * @param row   The row to be summed
      * @return      A string representing the formula
      */
-    public static String generateSumFormula(String sheet, int row) {
+    public static String generateSumFormula(String sheet, int column, int startRow, int endRow) {
     	StringBuilder formula = new StringBuilder();
     	formula.append("SUM(");
     	
-    	// Append columns B - Z
-    	for (int i = 0; i < 25; i++) {
-    		formula.append(sheet + "!" + (char)(66 + i) + row + ",");
+    	// Append rows
+    	for (int i = startRow; i < endRow; i++) {
+    		formula.append(sheet + "!" + (char)(65 + column) + i + ",");
     	}
     	
-    	// Append columns AA - AF
-    	formula.append(sheet + "!AA" + row + ",");
-    	formula.append(sheet + "!AB" + row + ",");
-    	formula.append(sheet + "!AC" + row + ",");
-    	formula.append(sheet + "!AD" + row + ",");
-    	formula.append(sheet + "!AE" + row + ",");
-    	formula.append(sheet + "!AF" + row + ")");
+    	// Append last row
+    	formula.append(sheet + "!" + (char)(65 + column) + endRow + ")");
     	
     	return formula.toString();
     }
